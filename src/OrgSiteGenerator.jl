a5ea9536c69fb7a08f9f2524c9b63c473ec49e71
Anthea Liles <120063455+AntheaLiles@users.noreply.github.com>
2024-08-07 14:43:56 +0000

    do garbage stuff

--- src/OrgSiteGenerator.jl
+++ src/OrgSiteGenerator.jl
@@ -1,5 +1,7 @@
 module OrgSiteGenerator
 
+using Genie
+
 include("../scripts/org_to_html.jl")
 include("../scripts/build_site.jl")
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using Test
 using OrgSiteGenerator
 
 @testset "OrgSiteGenerator Tests" begin
